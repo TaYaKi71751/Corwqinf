e9b7a3b6b784d9a9bc3b021fd053fc91ebb88bc7
TaYaKi71751 <shantian.tw@gmail.com>
2020-10-11 01:14:04 -0400

    Edit Request.java

--- src/main/java/koreawqi/temperature/Request.java
+++ src/main/java/koreawqi/temperature/Request.java
@@ -14,17 +14,20 @@ public class Request {
 
         String timetableString = timetableElements.get(0).childNode(3).childNode(0).toString();
         while (timetableString.contains("<") | timetableString.contains(">")) {
-            timetableString = timetableString
-                    .replace(timetableString.substring(timetableString.indexOf("<"), timetableString.indexOf(">")+1), "");
+            timetableString = timetableString.replace(
+                    timetableString.substring(timetableString.indexOf("<"), timetableString.indexOf(">") + 1), "");
         }
         timetableString = timetableString.substring(timetableString.indexOf("="), timetableString.indexOf(";"));
 
-        while(timetableString.contains(" ")||timetableString.contains("=")||timetableString.contains("\"")) timetableString = timetableString.replace("=", "").replace("\"", "").replace(" ", "");
+        while (timetableString.contains(" ") || timetableString.contains("=") || timetableString.contains("\""))
+            timetableString = timetableString.replace("=", "").replace("\"", "").replace(" ", "");
 
         String jsonDataString = "";
         jsonDataString += "{";
-        jsonDataString += "\"" + "date" + "\"" + ":" + "\"" + timetableString.substring(0,timetableString.indexOf("+")) + "\",";
-        jsonDataString += "\"" + "time" + "\"" + ":" + "\"" + timetableString.substring(timetableString.indexOf("+")+1,timetableString.length()) + "\",";
+        jsonDataString += "\"" + "date" + "\"" + ":" + "\"" + timetableString.substring(0, timetableString.indexOf("+"))
+                + "\",";
+        jsonDataString += "\"" + "time" + "\"" + ":" + "\""
+                + timetableString.substring(timetableString.indexOf("+") + 1, timetableString.length()) + "\",";
         for (int i = 0; i < table_01Elements.size(); i++) {
             jsonDataString += (i == 0 ? "\"Han\""
                     : i == 1 ? "\"Nakdong\"" : i == 2 ? "\"Geum\"" : i == 3 ? "\"Yeongsan\"" : "\"undefined\"") + ":[";
@@ -37,12 +40,16 @@ public class Request {
                     try {
                         jsonDataString += "{" + "\"name\":" + "\""
                                 + table_01Elements.get(i).childNode(j).childNode(k).childNode(1).childNode(0).toString()
-                                + "\"," + "" + "\"temp\":"
-                                + (table_01Elements.get(i).childNode(j).childNode(k).childNode(4).toString().contains(
-                                        ".") ? table_01Elements.get(i).childNode(j).childNode(k).childNode(4).toString()
-                                                : "\"" + table_01Elements.get(i).childNode(j).childNode(k).childNode(3)
-                                                        .childNode(0).toString() + "\"")
-                                + "" + "}";
+                                + "\"," + "" + "\"temp\":";
+                        jsonDataString += "\"";
+                        try {
+                            jsonDataString += table_01Elements.get(i).childNode(j).childNode(k).childNode(4).toString();
+                        } catch (Exception e) {
+                            jsonDataString += table_01Elements.get(i).childNode(j).childNode(k).childNode(3)
+                                    .childNode(0).toString();
+                        }
+                        jsonDataString += "\"";
+                        jsonDataString += "" + "}";
                     } catch (Exception e) {
                         continue;
                     }
